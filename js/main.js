3b6906af38b2e7e16b6f7a0884bf70ce4afc6ec0
Jinjae Lee <jinjaelee@Administrators-MacBook-Pro-681.local>
2019-05-07 17:22:25 +0200

    hammer js test 3

--- js/main.js
+++ js/main.js
@@ -20,3 +20,24 @@ function onRotate(ev) {
     logEvent(ev);
     requestElementUpdate();
 }
+
+function requestElementUpdate() {
+    if(!ticking) {
+        reqAnimationFrame(updateElementTransform);
+        ticking = true;
+    }
+}
+
+function updateElementTransform() {
+    var value = [
+        'translate3d(' + transform.translate.x + 'px, ' + transform.translate.y + 'px, 0)',
+        'scale(' + transform.scale + ', ' + transform.scale + ')',
+        'rotate3d('+ transform.rx +','+ transform.ry +','+ transform.rz +','+  transform.angle + 'deg)'
+    ];
+
+    value = value.join(" ");
+    el.style.webkitTransform = value;
+    el.style.mozTransform = value;
+    el.style.transform = value;
+    ticking = false;
+}
