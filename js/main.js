56abeedf884dd75404b0ad172a5796b018cb50af
Jinjae Lee <jinjaelee@Administrators-MacBook-Pro-681.local>
2019-05-07 14:03:52 +0200

    pinch, rotate fixed

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 var myElement = document.getElementById('myElement');
 
-// create a simple instance
-// by default, it only adds horizontal recognizers
-var mc = new Hammer(myElement);
+var mc = new Hammer.Manager(myElement);
 
-// listen to events...
-mc.on("panleft panright tap press", function(ev) {
-    myElement.textContent = ev.type +" gesture detected.";
+// create a pinch and rotate recognizer
+// these require 2 pointers
+var pinch = new Hammer.Pinch();
+var rotate = new Hammer.Rotate();
+
+// we want to detect both the same time
+pinch.recognizeWith(rotate);
+
+// add to the Manager
+mc.add([pinch, rotate]);
+
+
+mc.on("pinch rotate", function(ev) {
+    myElement.textContent += ev.type +" ";
 });
-
-mc.get('pinch').set({ enable: true });
-mc.get('rotate').set({ enable: true });
