e5878680d3e30bc2b893bdb174d2fa415570417e
Jinjae Lee <jinjaelee@Administrators-MacBook-Pro-681.local>
2019-05-07 15:14:03 +0200

    two js test

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 var myElement = document.getElementById('myElement');
 
 var mc = new Hammer.Manager(myElement);
@@ -25,12 +26,37 @@ mc.on("pinch rotate", function(event) {
     uipositionX = event.center.y
 });
 
-function setup() {
-    createCanvas(640, 480);
-}
+// function setup() {
+//     createCanvas(640, 480);
+// }
 
-function draw() {
-    translate(width / 2, height / 2);
-    rotate(angle);
-    rect(uipositionX, uipositionY, 55, 55);
-}
+
+// function draw() {
+//     translate(width / 2, height / 2);
+//     rotate(angle);
+//     rect(uipositionX, uipositionY, 55, 55);
+// }
+
+// Make an instance of two and place it on the page.
+var elem = document.getElementById('myElement');
+var params = { width: 285, height: 200 };
+var two = new Two(params).appendTo(elem);
+
+// two has convenience methods to create shapes.
+var circle = two.makeCircle(72, 100, 50);
+var rect = two.makeRectangle(213, 100, 100, 100);
+
+// The object returned has many stylable properties:
+circle.fill = '#FF8000';
+circle.stroke = 'orangered'; // Accepts all valid css color
+circle.linewidth = 5;
+circle.rotation = angle;
+
+rect.fill = 'rgb(0, 200, 255)';
+rect.opacity = 0.75;
+rect.noStroke();
+rect.rotation = angle;
+
+// Don't forget to tell two to render everything
+// to the screen
+two.update();
