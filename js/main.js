54a22c0575f5616f8f9694d4680b730616b3a07f
Jinjae Lee <jinjaelee@AdminisrsMBP681.lan>
2019-05-09 00:05:35 +0200

    dom update 16

--- js/main.js
+++ js/main.js
@@ -162,7 +162,7 @@ function onRotate(ev) {
     transform.angle = initAngle + ev.rotation;
     
     
-    if (initAngle < 40) {
+    if (transform.angle < 40) {
         animationB.play();
         animationA.pause();
         animationD.play();
